74488c613a8e0f4454d793b69943f469e3325bcb
nawon <dlskdnjs030425@naver.com>
2023-05-15 19:30:57 +0900

    first commit

---
+++ workspace/JAVASCRIPT-TUTORIAL/datatype_p/datatypes.js
@@ -0,0 +1,45 @@
+/* 
+자료형
+
+1.기본형, 단순형, 원시형
+-number
+-string
+-bollean
+-undefined
+-null
+-symbol
+
+2. 참조형(객체-object)
+
+
+*/
+
+//number
+//정수(int)와 실수(float) 구분이 없음
+var price = 10000;
+console.log(price);
+
+//string
+var myname = "테킷";
+console.log(myname);
+
+var myname2 = "멋사";
+console.log(myname2);
+
+var myname3 = "'멋쟁이'";
+console.log(myname3);
+
+var myname4 = '"사자"';
+console.log(myname4);
+
+//  boolean(대소문자 구문-소문자로 적어주어야 함)
+var isTrue = true;
+console.log(isTrue);
+
+var isFalse = false;
+console.log(isFalse);
+
+//undefined(선언은 했지만 할당하지 않은 변수)
+var a;
+console.log(a);
+
---
+++ workspace/JAVASCRIPT-TUTORIAL/objects_p/objects.js
@@ -0,0 +1,8 @@
+var student = {
+  grade: 1,
+  school: "lion school",
+};
+
+console.log(student);
+console.log(typeof student);
+
