98486c47b89f276871bca11a7082425a6cdce24c
2na1 <128816420+2na1@users.noreply.github.com>
2023-07-30 21:58:45 +0900

    Add files via upload

---
+++ React JS/#6/App(1).js
@@ -0,0 +1,29 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+  function byeFn() {
+    console.log("bye :(");
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byeFn;
+  }
+  //clean up
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
